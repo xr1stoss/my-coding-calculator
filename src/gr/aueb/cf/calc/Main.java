cb40765759e160d44c3119d125e5be85519a8134
Xristos Mpertes <xristosster@yahoo.gr>
2025-01-14 18:13:11 +0200

    project created

---
+++ src/gr/aueb/cf/calc/Main.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.calc;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub( int a, int b) {
+        return a - b;
+    }
+}
